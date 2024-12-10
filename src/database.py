42b6b21f716dd7c6e0e732e4505295af9ff482c9
Elizabeth-nene-oge <127535361+Elizabeth-nene-oge@users.noreply.github.com>
2024-12-10 08:06:07 +0000

    update db

--- src/database.py
+++ src/database.py
@@ -9,7 +9,15 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 
 
-DATABASE_URL = os.getenv("POSTGRES_URL")
+PSQL_USER = os.getenv("PSQL_USER")
+PSQL_PASSWORD = os.getenv("PSQL_PASSWORD")
+PSQL_HOST = os.getenv("PSQL_HOST")
+PSQL_DB_NAME = os.getenv("PSQL_DB_NAME")
+PSQL_PORT = os.getenv("PSQL_PORT")
+
+DATABASE_URL = f"postgresql://{PSQL_USER}:{PSQL_PASSWORD}@{PSQL_HOST}:{PSQL_PORT}/{PSQL_DB_NAME}"
+
+
 
 engine = create_engine(DATABASE_URL)
 sessionLocal = sessionmaker(bind=engine)
